eb812c9ce91fec30daf801d943f7ad74510ece18
biradarsatyam66-eng <biradarsatyam66@gmail.com>
2026-07-29 00:21:57 +0530

    new component image addes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from '../product/product.component';
-import { CommentComponent } from '../comment/comment.component'
+import { CommentComponent } from '../comment/comment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageComponent } from './image/image.component'
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CommentComponent
+    CommentComponent,
+    ImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
