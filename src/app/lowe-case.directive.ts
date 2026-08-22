7441e464a07ed45c0443a05971357a14fd78d5f6
biradarsatyam66-eng <biradarsatyam66@gmail.com>
2026-08-22 16:02:12 +0530

    lower case

--- src/app/lowe-case.directive.ts
+++ src/app/lowe-case.directive.ts
@@ -7,26 +7,58 @@ export class LoweCaseDirective {
 
   constructor() { }
 
+  // @HostListener('keyup',['$event'])
+  // onformet(eve:Event){
+  //   let ele = (eve.target as HTMLInputElement)
+  //   let val:string = ele.value
+  //   console.log(val);
+
+  //   if(val.length > 16){
+  //     val = val.substring(0,16)
+  //     console.log(val);
+  //     ele.value = val
+  //   }
+  //   this.chunk(val)
+  // }
+
+  // chunk(str:string){
+  //   let arr = []
+  //   for(let i = 0 ; i < str.length ; i = i + 4){
+  //     arr.push(str.slice(i, i + 4))
+  //   }
+  //   console.log(arr)
+  // }
+
+
   @HostListener('keyup',['$event'])
-  onformet(eve:Event){
-    let ele = (eve.target as HTMLInputElement)
-    let val:string = ele.value
+  formetcard(eve:Event){
+    let ele = eve.target as HTMLInputElement
+    let val = ele.value.replace(/\s/g, '');
     console.log(val);
 
     if(val.length > 16){
       val = val.substring(0,16)
-      console.log(val);
-      ele.value = val
     }
-    this.chunk(val)
+
+    if(/[^\d]/.test(val)){
+      ele.nextElementSibling?.classList.remove('d-none')
+  }else{
+      ele.nextElementSibling?.classList.add('d-none')
   }
 
-  chunk(str:string){
+    let result = this.onchunk(val, 4);
+    ele.value = result;
+  }
+
+
+  onchunk(str:string,limit:number){
     let arr = []
-    for(let i = 0 ; i < str.length ; i = i + 4){
-      arr.push(str.slice(i, i + 4))
+    for(let i = 0; i < str.length; i = i + limit){
+      arr.push(str.slice(i, i+limit))
     }
+    
     console.log(arr)
+    return arr.join(' ');
   }
 
 }
